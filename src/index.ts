e289aec5463c310fbb83a1a02598332ab2757996
FaonDev <124811747+FaonDev@users.noreply.github.com>
2023-04-24 16:18:48 -0300

    Tiny update

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,6 @@ import Express from 'express'
 export const expressApp = Express()
 expressApp.use(Express.static('public'))
 RegisterRoutes(expressApp)
-expressApp.listen(80)
+expressApp.listen(5850)
+
+console.log(`Running on http://localhost:5850`)
